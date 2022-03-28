191ceda8a4c3ffd41a7a080613c1bea109fef1a6
LachTomasz <lach.tomasz@gmail.com>
2022-03-28 20:39:18 +0200

    Initial commit

--- src/StaticInteger.java
+++ src/StaticInteger.java
@@ -6,17 +6,17 @@ public class StaticInteger {
         System.out.println("Wielkość liczby wyrażona w bitach: " + Integer.SIZE);
         int a = 179;
         System.out.println("a= " + a);
-        System.out.println("Zamiana liczby na łańcuch binarny: " + Integer.toBinaryString(a));
-        System.out.println("Zamiana liczby na łańcuch  ósemkowy: " + Integer.toOctalString(a));
-        System.out.println("Zamiana liczby na łańcuch szesnastkowy: " + Integer.toHexString(a));
-        System.out.println("...: " + Integer.toString(a));
-        System.out.println("...: " + Integer.toString(a, 4));
+        System.out.println("Reprezentacja liczby w łańcuchu jako binarnej: " + Integer.toBinaryString(a));
+        System.out.println("Reprezentacja liczby w łańcuchu  jako ósemkowej: " + Integer.toOctalString(a));
+        System.out.println("Reprezentacja liczby w łańcuchu jako szesnastkowej: " + Integer.toHexString(a));
+        System.out.println("Reprezentacja liczby w łańcuchu jako dziesiętnej: " + Integer.toString(a));
+        System.out.println("Reprezentacja liczby w łańcuchu w zalerzności od parametru \"RADIX\", tu w formacie czwórkowym: " + Integer.toString(a, 4));
         int b = Integer.parseInt("-177");
         System.out.println("b = " + b);
         int c = Integer.parseInt("1000" , 8);
         System.out.println("c = " + c);
-        System.out.println("...: " + Integer.signum(a));
-        System.out.println("...: " + Integer.signum(b));
-        System.out.println("...: " + Integer.signum(0));
+        System.out.println("Funkcja zwracająca informacje czy liczba jest dodatnia (1), ujemna (-1) czy wynosi zero (0): " + Integer.signum(a));
+        System.out.println("Funkcja zwracająca informacje czy liczba jest dodatnia (1), ujemna (-1) czy wynosi zero (0): " + Integer.signum(b));
+        System.out.println("Funkcja zwracająca informacje czy liczba jest dodatnia (1), ujemna (-1) czy wynosi zero (0): " + Integer.signum(0));
     }
 }
