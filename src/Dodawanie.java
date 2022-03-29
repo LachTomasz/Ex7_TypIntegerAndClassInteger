851a067cde37e534ed38418adcb21c9cc4789dee
LachTomasz <lach.tomasz@gmail.com>
2022-03-29 23:53:30 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/Dodawanie.java
@@ -0,0 +1,7 @@
+public class Dodawanie {
+    public static void main(String[] args) {
+        int a = 2354, b= 5667;
+        System.out.println("a = " + a +"\tb = " + b);
+        System.out.println("a + b = " + (a+b));
+    }
+}
