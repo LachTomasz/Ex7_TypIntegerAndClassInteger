80238d26c0d5ddf6c091c07053789adafe902998
LachTomasz <lach.tomasz@gmail.com>
2022-03-29 23:15:29 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/ObjectInteger.java
@@ -0,0 +1,20 @@
+public class ObjectInteger {
+    public static void main(String[] args) {
+        System.out.println("Wybrane metody obiektów klasy Integer\n");
+        /*
+        Tworzenie obiektów
+         */
+        Integer a = new Integer(1024);
+        Integer b = new Integer("02000");
+        Integer c = Integer.decode("02000");
+        Integer d = Integer.decode("0x2000");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        /*
+        Porównanie obiektów
+         */
+
+    }
+}
