672dd3c66e2402fb33db01d1863d7b93dece434e
LachTomasz <lach.tomasz@gmail.com>
2022-03-31 23:22:00 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/Mnożenie.java
@@ -0,0 +1,7 @@
+public class Mnożenie {
+    public static void main(String[] args) {
+        int a = 64, b = 35;
+        System.out.println("a = "+a+"\tb = "+b);
+        System.out.println("a * b = "+a*b);
+    }
+}
