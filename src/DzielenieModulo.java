754c4993b0ebca9308431d5fa6b429d1833de1b2
LachTomasz <lach.tomasz@gmail.com>
2022-03-31 23:31:33 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/DzielenieModulo.java
@@ -0,0 +1,7 @@
+public class DzielenieModulo {
+    public static void main(String[] args) {
+        int a = 5245, b = 2;
+        System.out.println("a = "+a+"\tb = "+b);
+        System.out.println("Reszta z dzielenia modulo \"%\": "+a%b);
+    }
+}
