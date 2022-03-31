ce4f78065eca822130dfe6ffb9baf9de55a739fc
LachTomasz <lach.tomasz@gmail.com>
2022-03-31 23:19:18 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/Odejmowanie.java
@@ -0,0 +1,8 @@
+public class Odejmowanie {
+    public static void main(String[] args) {
+        int a = 4563, b = 653;
+        System.out.println("a = "+a+"\tb = "+b);
+        System.out.print("a - b =");
+        System.out.println(a-b);
+    }
+}
