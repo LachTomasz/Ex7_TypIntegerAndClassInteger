91a826559d64bc6b62889c6d9c9cdae506ba13a3
LachTomasz <lach.tomasz@gmail.com>
2022-03-31 23:23:44 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/Dzielenie.java
@@ -0,0 +1,7 @@
+public class Dzielenie {
+    public static void main(String[] args) {
+        int a = 67234, b = 34;
+        System.out.println("a = "+a+"\tb + "+b);
+        System.out.println("a / b = "+a/b);
+    }
+}
