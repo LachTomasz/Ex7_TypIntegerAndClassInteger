7787bfdc1f6f89525d602bc3c37c7e7c1633e0fc
LachTomasz <lach.tomasz@gmail.com>
2022-04-03 00:40:40 +0200

    Wybrane metody obiektów klasy Integer

--- src/Zamiana.java
+++ src/Zamiana.java
@@ -1,9 +1,11 @@
 public class Zamiana {
     public static void main(String[] args) {
         try {
-        int numbers = Integer.parseInt(args[1]);
-        int radix = Integer.parseInt(args[0]);
-            System.out.println(Integer.toString(numbers,radix));
+            int numbers = Integer.parseInt(args[1]);
+            int radix = Integer.parseInt(args[0]);
+            System.out.println(args[1]+"[10] = "
+                    + Integer.toString(numbers, radix)
+                    + "["+radix+"]");
         } catch (Exception e){
             System.out.println("Podane parametry nie są zgodne z formatem. " +
                     "Program zakończy działanie.");
