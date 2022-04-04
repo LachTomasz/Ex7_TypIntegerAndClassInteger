3cb55107619a9bf5deba00afab6450568f5250b1
LachTomasz <lach.tomasz@gmail.com>
2022-04-04 21:28:12 +0200

    Wybrane metody obiektów klasy Integer

--- src/LongNumber.java
+++ src/LongNumber.java
@@ -4,14 +4,14 @@ public class LongNumber {
 
         System.out.println("n = "+n);
         System.out.println("BIN = "+Long.toBinaryString(n));
-        System.out.println("Hex = "+Long.toHexString(n)+"\n|");
+        System.out.println("Hex = "+Long.toHexString(n)+"\n");
 
         long m = n+1;
         System.out.println("m = "+m);
         System.out.println("BIN: "+Long.toBinaryString(m));
         System.out.println("HEX: "+Long.toHexString(m)+"\n");
 
-        Long max = new Long(n);
+        Long max = n;
         System.out.println("Zamiana na typ int. max = "+max.intValue());
         System.out.println("BIN: "+Integer.toBinaryString(max.intValue()));
         System.out.println("HEX: "+Integer.toHexString(max.intValue())+"\n");
