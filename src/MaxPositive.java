ac7ffbcb2ba7e5fe054d5985a2ed3effb1a06e22
LachTomasz <lach.tomasz@gmail.com>
2022-04-04 21:33:52 +0200

    Wybrane metody obiektów klasy Integer

---
+++ src/MaxPositive.java
@@ -0,0 +1,8 @@
+public class MaxPositive {
+    public static void main(String[] args) {
+        System.out.println("Wartości maxymalne typów:\n" +
+                        "\t\tbyte\t\t\t\tshort\t\t\t\t\t\t" +
+                "int\t\t\t\t\t\t\t\t\t\tlong ");
+        System.out.print("BIN");
+    }
+}
