cdba2339a1b6b8e95153e4dc286a133a6cec7d11
LachTomasz <lach.tomasz@gmail.com>
2022-04-04 22:12:52 +0200

    Wybrane metody obiektów klasy Integer

--- src/MaxPositive.java
+++ src/MaxPositive.java
@@ -1,8 +1,16 @@
+import com.sun.jdi.ByteValue;
+
 public class MaxPositive {
     public static void main(String[] args) {
-        System.out.println("Wartości maxymalne typów:\n" +
-                        "\t\tbyte\t\t\t\tshort\t\t\t\t\t\t" +
-                "int\t\t\t\t\t\t\t\t\t\tlong ");
-        System.out.print("BIN");
+        System.out.println("Wartości maxymalne w formacie:\n" +
+                        "\t\tHEX\t\t\t\t\tBIN");
+        System.out.print("BYTE:\t"+ Integer.toHexString(Byte.MAX_VALUE));
+        System.out.println("\t\t\t\t\t"+ Integer.toBinaryString(Byte.MAX_VALUE));
+        System.out.print("SHORT:\t"+ Integer.toHexString(Short.MAX_VALUE));
+        System.out.println("\t\t\t\t"+ Integer.toBinaryString(Short.MAX_VALUE));
+        System.out.print("INT:\t"+ Integer.toHexString(Integer.MAX_VALUE));
+        System.out.println("\t\t\t"+ Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.print("LONG:\t"+ Long.toHexString(Long.MAX_VALUE));
+        System.out.println("\t"+ Long.toBinaryString(Long.MAX_VALUE));
     }
 }
